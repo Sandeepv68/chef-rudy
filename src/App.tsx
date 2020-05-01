bb75df0fd86341cf1d545f62a53d4ab53cb86540
Sandeep Vattapparambil <sandeepv68@gmail.com>
2020-05-01 18:58:52 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import Welcome from "../assets/welcome.svg";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Image source={require("../assets/rudy.png")} style={styles.rudy}></Image>
-      <View>
-        <Text style={styles.primaryText}>Chef Rudy </Text>
-        <Text style={styles.secndaryText}>Welcomes you...</Text>
-      </View>
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/rudy.png")}
+            style={styles.rudy}
+          ></Image>
+        </View>
 
-      {/* <Welcome height={"300px"} /> */}
-    </View>
-  );
+        <View style={styles.headerTextBoxContainer}>
+          <Text style={styles.primaryText}>Chef Rudy </Text>
+          <Text style={styles.secondaryText}>Welcomes you...</Text>
+        </View>
+
+        {/* <Welcome height={"300px"} /> */}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -22,20 +30,36 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     backgroundColor: "#fff",
     display: "flex",
+    flexDirection: "row",
+  },
+  imageContainer: {
+    shadowOffset: { width: 10, height: 10 },
+    shadowColor: "black",
+    shadowOpacity: 1,
+    backgroundColor: "#0000",
   },
   rudy: {
     height: 80,
     width: 80,
     marginRight: 10,
+    backgroundColor: "#fff",
+    shadowOffset: { width: 10, height: 10 },
+    shadowColor: "#000",
+    shadowOpacity: 1,
   },
   primaryText: {
     fontSize: 19,
     fontWeight: "bold",
   },
-  secndaryText: {
+  secondaryText: {
     fontSize: 15,
     color: "#ccc",
     fontWeight: "100",
+  },
+  headerTextBoxContainer: {
+    display: "flex",
+    alignContent: "center",
+    paddingTop: 20,
   },
 });
 
