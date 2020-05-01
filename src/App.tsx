8fed88e7a4fe29278f2d4c6883bfb8233f7f86ba
Sandeep Vattapparambil <sandeepv68@gmail.com>
2020-05-01 18:04:59 +0530

    updates on skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Welcome from "../assets/welcome.svg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Chef Rudy</Text>
+      <Image source={require("../assets/rudy.png")} style={styles.rudy}></Image>
+      <View>
+        <Text style={styles.primaryText}>Chef Rudy </Text>
+        <Text style={styles.secndaryText}>Welcomes you...</Text>
+      </View>
+
+      {/* <Welcome height={"300px"} /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding: 10,
+    paddingTop: 40,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    display: "flex",
+  },
+  rudy: {
+    height: 80,
+    width: 80,
+    marginRight: 10,
+  },
+  primaryText: {
+    fontSize: 19,
+    fontWeight: "bold",
+  },
+  secndaryText: {
+    fontSize: 15,
+    color: "#ccc",
+    fontWeight: "100",
   },
 });
+
+const palette = {};
