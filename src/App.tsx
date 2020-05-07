fc0611d2d18472bf06fbc24fe8a2ae0dc0729dec
Sandeep Vattapparambil <sandeepv68@gmail.com>
2020-05-08 00:09:53 +0530

    documentation updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
+/**
+ * App
+ * Moin app component
+ */
+
+/**
+ * Import React and Redux
+ */
 import React from "react";
 import { Provider } from "react-redux";
 
+/**
+ * Import react-navigation
+ * and create a stack navigation component
+ */
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
 
+/**
+ * Import all the screens
+ */
 import HomeScreen from "./Screens/Home/HomeScreen";
 import RecipeSearch from "./Screens/RecipeSearch/RecipeSearch";
 import AnalyzeMeal from "./Screens/AnalyzeMeal/AnalyzeMeal";
 import ShowRecipe from "./Screens/ShowRecipe/ShowRecipe";
 
+/**
+ * Import the redux store and create a new store instance
+ */
 import chefRudyStore from "./Redux/Store/chefRudyStore";
 const store = chefRudyStore();
 
-function App() {
+/**
+ * App
+ * The main app component with screens wrapped in navigation HOC
+ */
+const App: React.SFC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -28,9 +50,13 @@ function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-const ChefRudy = () => (
+/**
+ * ChefRudy
+ * The app component with redux store provided
+ */
+const ChefRudy: React.SFC = () => (
   <Provider store={store}>
     <App />
   </Provider>
