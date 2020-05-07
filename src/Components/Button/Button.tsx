fe5f91258700bfe2649155373bc2f6109b04d6e1
Sandeep Vattapparambil <sandeepv68@gmail.com>
2020-05-07 18:00:37 +0530

    refactored button component to accommodate typescript featueres

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,14 +1,30 @@
+/**
+ * CustomButton
+ * A reusable customizable button component
+ */
+
+/**
+ * Import React and React Native components
+ */
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+
+/**
+ * Import Interfaces and stylesheet required for the component
+ */
+import { CustomButtonProps } from "./CustomButtonProps";
 import { styles } from "./style";
 
-export const CustomButton = (props: any) => {
+/**
+ * CustomButton
+ * A customizable , reusable button component
+ * @param props - The interface <CustomButtonProps>
+ */
+export const CustomButton: React.SFC<CustomButtonProps> = (props) => {
   return (
     <TouchableOpacity onPress={props.onPress}>
       <View style={{ ...styles.buttonStyleBase, ...props.styles }}>
-        <Text
-          style={{ ...styles.buttonTextStyleBase, ...props.buttonTextStyle }}
-        >
+        <Text style={{ ...styles.buttonTextStyleBase, ...props.buttonTextStyle }}>
           {props.children}
         </Text>
       </View>
---
+++ src/Components/Button/CustomButtonProps.d.ts
@@ -0,0 +1,7 @@
+export interface CustomButtonProps {
+  onPress: VoidFunction;
+  styles: Object;
+  buttonStyleBase: Object;
+  buttonTextStyleBase: Object;
+  buttonTextStyle: Object;
+}
